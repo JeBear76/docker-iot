17d6c5b4fa0febb51761f5b899d2ed63e86b8893
Jebear76 <jerepondumie@gmail.com>
2024-07-08 06:30:26 +0200

    changed certificates

---
+++ docker-iot-device-config/configure-iot-device.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+curl -o ~/certs/Amazon-root-CA-1.pem https://www.amazontrust.com/repository/AmazonRootCA1.pem 
+
+CERTIFICATE_ARN=$(aws iot create-keys-and-certificate \
+    --profile iot-user \
+    --no-cli-pager \
+    --set-as-active \
+    --certificate-pem-outfile "~/certs/docker-iot-thing.pem.crt" \
+    --public-key-outfile "~/certs/docker-iot-thing-public.pem.key" \
+    --private-key-outfile "~/certs/docker-iot-thing-private.pem.key" \
+    --query certificateArn | tr -d '"')
+
+aws iot create-thing \
+    --profile iot-user \
+    --no-cli-pager \
+    --thing-name "docker-iot-thing"
+
+aws iot attach-thing-principal \
+    --profile iot-user \
+    --no-cli-pager \
+    --thing-name "docker-iot-thing" \
+    --principal $CERTIFICATE_ARN
+
+aws iot create-policy \
+    --profile iot-user \
+    --no-cli-pager \
+    --policy-name "docker-iot-thing-policy" \
+    --policy-document "file://./docker-iot-device-config/policy.json"
